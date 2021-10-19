70adcc5636c13f693e852200b012672715a28e70
manfred-commits <manfredipiraino@gmail.com>
2021-10-19 15:16:59 +0200

    added get variables functioning with if condition

--- snack-2/index.php
+++ snack-2/index.php
@@ -2,12 +2,14 @@
 Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
 
 <?php
-// $name_parameter=$GET_['name'];
-// $mail_parameter=$GET_['mail'];
-// $age_parameter=$GET_['age'];
-$name_parameter='manfredi';
-$mail_parameter='manfredipiraino@gmail.com';
-$age_parameter=25;
+$name_parameter=$_GET['name'];
+$mail_parameter=$_GET['mail'];
+$age_parameter=$_GET['age'];
+
+// basic debug static
+// $name_parameter='manfredi';
+// $mail_parameter='manfredipiraino@gmail.com';
+// $age_parameter=25;
 
 if(strlen($name_parameter)>3 && strpos($mail_parameter,".") && strpos($mail_parameter,"@") && is_numeric($age_parameter)){
     echo "Accesso riuscito";
