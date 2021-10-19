97c7274bc37039fadc4a30f351eddfb6c6787dc0
manfred-commits <manfredipiraino@gmail.com>
2021-10-19 16:33:16 +0200

    added css formatting to page

--- snack-2/index.php
+++ snack-2/index.php
@@ -27,27 +27,40 @@ if(!empty($name_parameter) && !empty($mail_parameter) && !empty($age_parameter))
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- link to font family -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" rel="stylesheet">
+        <!-- /link to font family -->
+    <!-- link to ccss -->
+    <link rel="stylesheet" href="./css/style.css">
+    <!-- /link to ccss -->
+
     <title>Snack-2</title>
 </head>
 <body>
-    <form action="">
-        <input type="text" name="name">
-        <input type="text" name="mail">
-        <input type="text" name="age">
-        <input type="submit">
-    </form>
-    
-    <?php if(!empty($name_parameter) && !empty($mail_parameter) && !empty($age_parameter)){?>
+    <section class="main-container">
+        <form action="">
+            <input type="text" name="name" placeholder="Inserisci Nome Utente">
+            <input type="text" name="mail" placeholder="Inserisci Email">
+            <input type="text" name="age" placeholder="Inserisci Età">
+            <div id="id="input-btn>
+                <input type="submit">
+            </div>
+        </form>
         
-        <?php if($verified){?>
-        <h2>Acesso riuscito</h2>
+        <?php if(!empty($name_parameter) && !empty($mail_parameter) && !empty($age_parameter)){?>
+            
+            <?php if($verified){?>
+            <h2>Acesso riuscito</h2>
+            <?php }else{?>
+            <h2>Acesso Negato</h2>
+            <?php }?>
+    
         <?php }else{?>
-        <h2>Acesso Negato</h2>
+            <h2 class="general-message">Compila i campi per effettuare l'accesso</h2>
         <?php }?>
-
-    <?php }else{?>
-        <h2>Compila i campi per effettuare l'accesso</h2>
-    <?php }?>
+    </section>
 
 
 </body>
