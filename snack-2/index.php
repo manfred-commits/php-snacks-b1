1ba8543da8464161d612f9db6a812f56c251ace2
manfred-commits <manfredipiraino@gmail.com>
2021-10-19 15:20:19 +0200

    added basic condition for execution of main code

--- snack-2/index.php
+++ snack-2/index.php
@@ -2,17 +2,23 @@
 Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
 
 <?php
+
 $name_parameter=$_GET['name'];
 $mail_parameter=$_GET['mail'];
 $age_parameter=$_GET['age'];
 
-// basic debug static
-// $name_parameter='manfredi';
-// $mail_parameter='manfredipiraino@gmail.com';
-// $age_parameter=25;
+if(!empty($name_parameter) && !empty($mail_parameter) && !empty($age_parameter)){
+    // basic debug static
+    // $name_parameter='manfredi';
+    // $mail_parameter='manfredipiraino@gmail.com';
+    // $age_parameter=25;
+    
+    if(strlen($name_parameter)>3 && strpos($mail_parameter,".") && strpos($mail_parameter,"@") && is_numeric($age_parameter)){
+        echo "Accesso riuscito";
+    }else{
+        echo "Accesso negato";
+    }
 
-if(strlen($name_parameter)>3 && strpos($mail_parameter,".") && strpos($mail_parameter,"@") && is_numeric($age_parameter)){
-    echo "Accesso riuscito";
 }else{
-    echo "Accesso negato";
+    echo "Inserisci Nome, Email e età";
 }
