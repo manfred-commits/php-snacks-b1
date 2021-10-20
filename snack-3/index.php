523174003be0f004ba403a6724fca7969f7b89cd
manfred-commits <manfredipiraino@gmail.com>
2021-10-20 11:56:57 +0200

    fine snack 3

--- snack-3/index.php
+++ snack-3/index.php
@@ -1,5 +1,5 @@
 <?php
-
+// Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
 $posts = [
 
     '10/01/2019' => [
@@ -39,3 +39,45 @@ $posts = [
         ]
     ],
 ];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- link to font family -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" rel="stylesheet">
+        <!-- /link to font family -->
+    <!-- link to ccss -->
+    <link rel="stylesheet" href="./css/style.css">
+    <!-- /link to css -->
+    <title>Snack-3</title>
+</head>
+<body>
+    <section class="main-container">
+        
+        
+        <?php foreach($posts as $postDate => $postValue){?>
+            
+            <h2><?php echo $postDate;?></h2>
+            <ul>
+            <?php foreach($postValue as $value){?>
+                
+                <li>
+                <?php foreach($value as $title=>$content){?>
+                    <?php echo "{$title} : {$content} <br>";?> 
+                <?php }?>         
+                </li>
+            <?php }?>
+            </ul>
+    
+        <?php }?>
+    </section>
+
+
+</body>
+</html>
